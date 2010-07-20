ee39fef7167d05013d9addd13efc650c1ffa247c
Andrew Radev <andrey.radev@gmail.com>
2010-07-20 21:48:17 +0300

    Gimp floated by default
    Apparently, setting it in an IM layout is not as useful as I thought.

--- lib/Manage.hs
+++ lib/Manage.hs
@@ -10,8 +10,8 @@ customManageHook = manageDocks
   <+> composeAll
     [ className =? "Skype"  --> doF (W.shift "2")
     , className =? "Pidgin" --> doF (W.shift "2")
-    , className =? "Gimp"   --> doF (W.shift "0")
-    , className =? "Gimp"   --> unfloat
+--    , className =? "Gimp"   --> doF (W.shift "0")
+    , className =? "Gimp"   --> doFloat
     , className =? "Zenity" --> doFloat
     -- fix flash fullscreen
     , isFullscreen --> (doF W.focusDown <+> doFullFloat <+> doF W.focusUp)
